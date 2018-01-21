90337726a79020af6fa6121709faf23181acdd23
ob-ivan <ob-ivan@noreply.users.github.com>
2018-01-20 16:22:03 -0800

    Move html to a template

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -1,10 +1,12 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class LuckyController
+
+class LuckyController extends Controller
 {
     /**
      * @Route("/lucky/number")
@@ -13,8 +15,8 @@ class LuckyController
     {
         $number = mt_rand(0, 100);
 
-        return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
-        );
+        return $this->render('lucky/number.html.twig', array(
+            'number' => $number,
+        ));
     }
 }
