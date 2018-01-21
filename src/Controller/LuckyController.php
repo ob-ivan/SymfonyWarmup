c5c2decbaa74489ed6a92357386f21a8a3130ad6
ob-ivan <ob-ivan@noreply.users.github.com>
2018-01-20 16:04:54 -0800

    Add route annotation

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -2,9 +2,13 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class LuckyController
 {
+    /**
+     * @Route("/lucky/number")
+     */
     public function number()
     {
         $number = mt_rand(0, 100);
