3f6c89c744745347b8146b5bcf861700a410ed0e
ob-ivan <ob-ivan@noreply.users.github.com>
2018-01-22 02:12:53 -0800

    Add more fields

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -16,5 +16,13 @@ class Product
      */
     private $id;
 
-    // add your own fields
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="decimal", scale=2, nullable=true)
+     */
+    private $price;
 }
