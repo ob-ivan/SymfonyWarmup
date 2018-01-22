0455bf0ba697c9a7f9d1b25aee1db69872fc48f1
ob-ivan <ob-ivan@noreply.users.github.com>
2018-01-22 02:31:21 -0800

    Fix import

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\Product;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
