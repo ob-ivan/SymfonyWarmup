acbc3ea566c6e2ad67aac0717c7e7e1a4c848815
ob-ivan <ob-ivan@noreply.users.github.com>
2018-01-22 03:48:59 -0800

    Create task controller

---
+++ src/Controller/TaskController.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Controller;
+
+use App\Entity\Task;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class TaskController extends Controller
+{
+    public function newAction(Request $request)
+    {
+        // create a task and give it some dummy data for this example
+        $task = new Task();
+        $task->setTask('Write a blog post');
+        $task->setDueDate(new \DateTime('tomorrow'));
+
+        $form = $this->createFormBuilder($task)
+            ->add('task', TextType::class)
+            ->add('dueDate', DateType::class)
+            ->add('save', SubmitType::class, array('label' => 'Create Task'))
+            ->getForm();
+
+        return $this->render('task/new.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
+}
